04d43dc8cb78cda106ba63e5f10539f1e09d07a4
AlayandeWork <alayandework@gmail.com>
2023-07-25 19:46:40 +0100

    Godot Game

---
+++ Nodes/Hero/Hero.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+@export var speed : int = 80
+@export var friction : int = 500
+@export var acceleration : int = 500
+
+enum {
+	MOVE,
+	ATTACK,
+}
+var state = MOVE
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+	
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	input_vector.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * speed, acceleration * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	set_velocity(velocity)
+	move_and_slide()
+	velocity = velocity
+	
+	if Input.is_action_just_pressed("Attack"):
+		state = ATTACK
+		
+		
+func attack_state(_delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
+	
