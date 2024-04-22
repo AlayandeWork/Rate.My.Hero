239f2fcf181cfc4fed49934974de72fc7c3c8ce3
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-21 22:08:20 -0400

    Background music, button sounds and looping

--- Nodes/Menu/menu.gd
+++ Nodes/Menu/menu.gd
@@ -1,15 +1,38 @@
 extends Control
 
+@onready var scene_timer: Timer = $SceneTimer
+
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
-
+	
+	
+# PRESSED AREA
 func _on_start_button_pressed():
+	$SelectingMenu.play()
 	get_tree().change_scene_to_file("res://Nodes/Main/Main.tscn")
 
-
 func _on_options_menu_pressed():
-	pass # Replace with function body.
-
+	$SelectingMenu.play()
+	# Replace with function body.
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+	$SelectingMenu.play()
+	
+	
+# HOVER AREA
+func _on_start_button_focus_entered():
+	$MovingMenu.play()
+	# Replace with function body.
+
+func _on_options_menu_focus_entered():
+	$MovingMenu.play()
+	# Replace with function body.
+
+func _on_exit_button_focus_entered():
+	$MovingMenu.play()
+	# Replace with function body.
+
+
+func _on_background_menu_finished():
+	$BackgroundMenu.play()
