ff12f2928061ca25e7d378ad347b0cb03589080e
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-21 20:51:30 -0400

    added keyboard navigation to the main menu

--- Nodes/Menu/menu.gd
+++ Nodes/Menu/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	pass
+	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Nodes/Main/Main.tscn")
