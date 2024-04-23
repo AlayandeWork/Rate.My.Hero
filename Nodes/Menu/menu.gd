ec518c949f6b9767d1f1c286726263fd1e701b2e
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-23 15:30:05 -0400

    added the back button functionality

--- Nodes/Menu/menu.gd
+++ Nodes/Menu/menu.gd
@@ -13,6 +13,7 @@ func _on_start_button_pressed():
 
 func _on_options_menu_pressed():
 	$SelectingMenu.play()
+	get_tree().change_scene_to_file("res://Nodes/Menu/options.tscn")
 	# Replace with function body.
 
 func _on_exit_button_pressed():
