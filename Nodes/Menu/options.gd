83c6ef6971013eb558645aa7a0976739cf5d08aa
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-26 19:21:54 -0400

    Adjusted Node Scene Call

--- Nodes/Menu/options.gd
+++ Nodes/Menu/options.gd
@@ -1,9 +1,9 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
 func _on_back_button_pressed():
@@ -13,7 +13,7 @@ func _on_back_button_pressed():
 
 
 func _on_back_button_mouse_entered():
-	$MovingMenu.play() # Replace with function body.
+	$MovingMenu.play() 
 
 
 func _on_back_button_focus_entered():
