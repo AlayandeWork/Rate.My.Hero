8849ed7cad607c1fe155fe93821811f03543fcaa
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-28 13:50:36 -0400

    Escape button to go the main menu

--- Nodes/Main/Main.gd
+++ Nodes/Main/Main.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready():
 	AudioPlayer._stop_music()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Escape"):
+		get_tree().change_scene_to_file("res://Nodes/Menu/menu.tscn")
