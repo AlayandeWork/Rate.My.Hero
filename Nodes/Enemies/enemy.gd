5d0e93ad2d1aab8de25757b4e976247e35294b41
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-05-06 02:05:20 -0400

    basic enemy path following

--- Nodes/Enemies/enemy.gd
+++ Nodes/Enemies/enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-var SPEED = 15
+var SPEED = 40
 var CHASING_PLAYER = false
 var PLAYER = null
 
@@ -8,7 +9,14 @@ var PLAYER = null
 func _physics_process(delta):
 	if CHASING_PLAYER:
 		position += (PLAYER.position - position)/SPEED
+		animated_sprite_2d.play("enemy_run")
 		
+		if (PLAYER.position.x - position.x)<0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
+	else:
+		animated_sprite_2d.play("enemy_idle")
 
 func _on_detection_body_entered(body):
 	PLAYER = body
