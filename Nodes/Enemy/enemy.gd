ea906efb3a5c671d0599af7ed83c557015562ecd
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-05-10 15:27:53 -0400

    created instances of player and enemy in the main

--- Nodes/Enemy/enemy.gd
+++ Nodes/Enemy/enemy.gd
@@ -4,5 +4,15 @@ var speed = 40
 var friction = 100
 var is_chasingPlayer = false
 
-func _physics_process(delta):
+#func _physics_process(delta):
+
+
 	
+
+
+func _on_detection_area_2d_body_entered(body):
+	print("Entered")
+
+
+func _on_detection_area_2d_body_exited(body):
+	print("Exit")
