847cb1931a18583ef06acea6566633bba8d66d4e
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-05-10 15:47:53 -0400

    QOL Changes

--- Nodes/Enemy/enemy.gd
+++ Nodes/Enemy/enemy.gd
@@ -2,17 +2,25 @@ extends CharacterBody2D
 
 var speed = 40
 var friction = 100
+var acceleration = 100
 var is_chasingPlayer = false
+var player = null
+var target_posiiton = player.position
 
-#func _physics_process(delta):
 
-
-	
-
+func _physics_process(delta):
+	if is_chasingPlayer:
+		pass
+	else:
+		pass
 
 func _on_detection_area_2d_body_entered(body):
+	player = body
+	is_chasingPlayer=true
 	print("Entered")
 
 
 func _on_detection_area_2d_body_exited(body):
+	player=null
+	is_chasingPlayer=false
 	print("Exit")
