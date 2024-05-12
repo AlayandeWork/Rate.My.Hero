f11c9c84e22e4a0ef5762cc1233f6be84301dd64
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-05-12 14:07:33 -0400

    Set Autoload level to main level only

--- Nodes/Enemy/enemy.gd
+++ Nodes/Enemy/enemy.gd
@@ -1,26 +1,26 @@
 extends CharacterBody2D
 
-var speed=40
+var speed=10
 var friction=20
 var acceleration=30
 var player=null
 var isChasing=false
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 func _physics_process(delta):
 	if isChasing:
-		position=position.move_toward(player.position, speed * delta)
+		position += (player.position - position)/speed
+		animated_sprite_2d.play("run")
 	else:
-		
-
+		animated_sprite_2d.play("idle")
 
 func _on_detection_area_2d_body_entered(body):
-	player==body
+	player=body
 	isChasing=true
 	print("Entered")
 
-
 func _on_detection_area_2d_body_exited(body):
-	player==null
+	player=null
 	isChasing=false
 	print("Exit")
