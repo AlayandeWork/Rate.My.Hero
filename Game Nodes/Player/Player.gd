550dd5621cb5506d721a00062491e3cb89e1f7d9
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-05-17 06:39:20 -0400

    Restructuring player code

--- Game Nodes/Player/Player.gd
+++ Game Nodes/Player/Player.gd
@@ -34,11 +34,11 @@ func _ready():
 func _physics_process(delta):
 	#enemyattacking()
 	
-	if health<=0:
-		playalive=false
-		health=0
-		print("player is dead")
-		queue_free()
+	#if health<=0:
+		#playalive=false
+		#health=0
+		#print("player is dead")
+		#queue_free()
 		
 	# CHECKS THE STATE VARIABLE AND CALLS THE APPROPRIATE STATE WHEN NEEDED	
 	match state:
@@ -75,24 +75,24 @@ func attack_state(_delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 		
-func attack_animation_finished():
-	GameManager.player_is_attacking=false
-	state = MOVE
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("enemy"):
-		enemyinside=true
-
-func _on_area_2d_body_exited(body):
-	if body.is_in_group("enemy"):
-		enemyinside=false
-
-func enemyattacking():
-	if enemyinside and enemyattackcooldown==true:
-		health = health - 10
-		enemyattackcooldown=false
-		player_cooldown.start()
-		print(health)
-
-func _on_god_mode_timeout():
-	enemyattackcooldown=true
+#func attack_animation_finished():
+	#GameManager.player_is_attacking=false
+	#state = MOVE
+#
+#func _on_area_2d_body_entered(body):
+	#if body.is_in_group("enemy"):
+		#enemyinside=true
+#
+#func _on_area_2d_body_exited(body):
+	#if body.is_in_group("enemy"):
+		#enemyinside=false
+#
+#func enemyattacking():
+	#if enemyinside and enemyattackcooldown==true:
+		#health = health - 10
+		#enemyattackcooldown=false
+		#player_cooldown.start()
+		#print(health)
+#
+#func _on_god_mode_timeout():
+	#enemyattackcooldown=true
